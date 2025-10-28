9b0d2581c42f363052eeff12bed2f87aa95a9942
JayaKaviya <kavyamanz@gmail.com>
2025-10-28 20:52:01 +0530

    Welcome page buttons added

--- client/myapp/src/components/Hero.jsx
+++ client/myapp/src/components/Hero.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './Hero.css'
+import { useNavigate } from 'react-router-dom'
 function Hero() {
+    const navigate=useNavigate();
   return (
     <div className='my-div'>
       <div className='intro'>
             <h1 className='my-heading'>Create Smarter Content <br/> with <span className='ai'>AI tool</span></h1>
             <p className='my-paragraph'>Boost your content creation using our advanced AI tools. Craft engaging articles, generate stunning visuals, and streamline your workflow.</p> 
+            <div className="button-container">
+                    <button className="button-primary"onClick={() => navigate('/ai')} >
+                        Start creating now
+                    </button>
+                    <button className="button-secondary">
+                        Watch demo
+                    </button>
+            </div>
         </div>
     </div>
   )
