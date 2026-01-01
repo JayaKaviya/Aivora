46da25dbdcc44f03452bd38b885700a81ade76bf
JayaKaviya <kavyamanz@gmail.com>
2026-01-01 20:59:49 +0530

    Blob titles page content updated

--- client/myapp/src/pages/BlogTitle.jsx
+++ client/myapp/src/pages/BlogTitle.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
-
+import './WriteArticle.css'
+import { Sparkles,Edit, Hash } from 'lucide-react';
+import { useState } from 'react';
 function BlogTitle() {
+
+   const blobCategories=[
+   'General', 'Technology', 'Business', 'Health','Lifestyle', 'Education',
+   'Travel','Food'] 
+
+  const [selectedCategory,setSelectedCategory]= useState('General');
+  const [input,setInput]= useState('');
+
+  const onSubmitHandler=async(e)=>{
+          e.preventDefault();
+    }
+
   return (
-    <div>
-      <p>BlogTitle</p>
+    <div className="article-container">
+          <form className="left-card" onSubmit={onSubmitHandler}>
+                <div className="card-header">
+                  <Sparkles className="blob-icon" />
+                  <h1 className="card-title">AI Title Generator</h1>
+                </div>
+                <p className="form-label">Keyword</p>
+                <input type="text" className="form-input" onChange={(e)=>setInput(e.target.value)} value={input} placeholder="The future of artificial intelligence is..." required />        
+                <p className="form-label label-spacing">Category</p>
+                <div className='length-container'>
+                  {
+                    blobCategories.map((item) => (
+                      <span onClick={() => setSelectedCategory(item)} key={item}
+                       className={`length-chip ${selectedCategory === item ? 'length-blob-active' : ''}`}>{item}</span>
+                    ))
+                  }
+                </div>
+                <br />
+               <button className="generate-button">
+                  <Hash className="generate-icon" />
+                  Generate title
+                </button>
+
+          </form>
+          <div className="right-column-blob">
+              <div className="generated-header">
+                <Hash className="header-icon-blob" />
+                <h1 className="generated-title">Generated titles</h1>
+              </div>
+
+              <div className="generated-body">
+                <div className="generated-empty">
+                  <Hash className="empty-icon" />
+                  <p>Enter a topic and click "Generated title" to get started</p>
+                </div>
+              </div>
+          </div>
+
     </div>
   )
 }
--- client/myapp/src/pages/WriteArticle.jsx
+++ client/myapp/src/pages/WriteArticle.jsx
@@ -1,17 +1,61 @@
 import React from 'react'
 import './WriteArticle.css'
-import { Sparkles } from 'lucide-react';
-
+import { Sparkles,Edit } from 'lucide-react';
+import { useState } from 'react';
 function WriteArticle() {
+  
+  const articleLength=[
+    {length:800,text:'Short (500-800 words)'},
+    {length:1200,text:'Medium (800-1200 words)'},
+    {length:1600,text: 'Long (1200+ words)'}
+
+  ] 
+
+  const [selectedLength,setSelectedLength]= useState(articleLength[0]);
+  const [input,setInput]= useState('');
+  const onSubmitHandler=async()=>{
+        e.preventDefault();
+  }
+
   return (
     <div className="article-container">
-          <form className="left-card">
+          <form className="left-card" onSubmit={onSubmitHandler}>
                 <div className="card-header">
-                  <Sparkles className="icon" />
+                  <Sparkles className=" blob icon" />
                   <h1 className="card-title">Article Configuration</h1>
                 </div>
+                <p className="form-label">Article Topic</p>
+                <input type="text" className="form-input" onChange={(e)=>setInput(e.target.value)} value={input} placeholder="The future of artificial intelligence is..." required />        
+                <p className="form-label label-spacing">Article Length</p>
+                <div className='length-container'>
+                  {
+                    articleLength.map((item,index) => (
+                      <span onClick={() => setSelectedLength(item)} key={index}
+                       className={`length-chip ${selectedLength?.text === item.text ? 'length-chip-active' : ''}`}>{item.text}</span>
+                    ))
+                  }
+                </div>
+                <br />
+               <button className="generate-button">
+                  <Edit className="generate-icon" />
+                  Generate article
+                </button>
+
           </form>
-      <div className="right-column"></div>
+          <div className="right-column">
+              <div className="generated-header">
+                <Edit className="header-icon" />
+                <h1 className="generated-title">Generated Article</h1>
+              </div>
+
+              <div className="generated-body">
+                <div className="generated-empty">
+                  <Edit className="empty-icon" />
+                  <p>Enter a topic and click "Generate article" to get started</p>
+                </div>
+              </div>
+          </div>
+
     </div>
   )
 }
