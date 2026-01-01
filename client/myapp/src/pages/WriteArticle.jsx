00bf7c0a79fe8e59502dafdad3faa7f46d891fe4
JayaKaviya <kavyamanz@gmail.com>
2026-01-01 15:58:03 +0530

    Added left and right column

--- client/myapp/src/pages/WriteArticle.jsx
+++ client/myapp/src/pages/WriteArticle.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './WriteArticle.css'
+import { Sparkles } from 'lucide-react';
 
 function WriteArticle() {
   return (
-    <div>
-      <p>WriteArticle</p>
+    <div className="article-container">
+          <form className="left-card">
+                <div className="card-header">
+                  <Sparkles className="icon" />
+                  <h1 className="card-title">Article Configuration</h1>
+                </div>
+          </form>
+      <div className="right-column"></div>
     </div>
   )
 }
