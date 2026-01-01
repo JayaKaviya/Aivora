73d9b825750a6224198991272dff2965143194ed
JayaKaviya <kavyamanz@gmail.com>
2026-01-01 14:56:51 +0530

    Recent Creations added

---
+++ client/myapp/src/components/CreationItem.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './CreationItem.css'
+import { useState } from 'react'
+function CreationItem({item}) {
+
+    const [expanded,setExpanded]=useState(false)
+
+  return (
+    <div onClick={()=> setExpanded(!expanded)}className="creation-item">    
+       <div  className="creation-header">
+           <div>
+                <h2>{item.prompt}</h2> 
+                <p className='item-meta'>{item.type} - {new Date(item.created_at).toLocaleDateString()}</p>          
+            </div>
+            <button className='item-type-btn'>{item.type}</button>
+       </div>
+
+       {
+        expanded && (
+            <div>
+                {item.type === 'image'? 
+                    (
+                        <div>
+                            <img src={item.content} alt="image" className='container-md'/>
+                        </div>
+                    ) : 
+                    (
+                        <div className='content-box'>
+                            <div>
+                                {item.content}                         
+                            </div>
+                        </div>
+                    )
+            }
+            </div>
+        )
+       }
+    </div>
+  )
+}
+
+export default CreationItem
--- client/myapp/src/pages/Dashboard.jsx
+++ client/myapp/src/pages/Dashboard.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { dummyCreationData } from '../assets/assets'
 import './Dashboard.css'
 import { useEffect,useState } from 'react'
-import { Sparkles } from 'lucide-react';
+import { Sparkles,Gem } from 'lucide-react';
+import {Protect} from '@clerk/clerk-react'
+import CreationItem from '../components/CreationItem';
 
 const Dashboard = () => {
 
@@ -19,35 +21,42 @@ const Dashboard = () => {
 
       return (
        <div className="scroll-container">
-          <div className="cards-wrapper">
-            {/* Total Creations Card */}
-            <div className="card">
-                <div className="card-text">
-                    <p className="card-label">Total Creations</p>
-                    <h2 className="card-number">{creations.length}</h2>              
-                </div>
-                <div className="card-icon">
-                    <Sparkles className="sparkles-icon" />
+            <div className="cards-wrapper">
+              {/* Total Creations Card */}
+                <div className="card">
+                    <div className="card-text">
+                        <p className="card-label">Total Creations</p>
+                        <h2 className="card-number">{creations.length}</h2>              
+                    </div>
+                    <div className="card-icon">
+                        <Sparkles className="sparkles-icon" />
+                    </div>
                 </div>
             </div>
-          </div>
 
-          <div className="cards-wrapper">
-            {/* Active Plan Card */}
-            <div className="card">
-                <div className="card-text">
-                    <p className="card-label">Active Plan</p>
-                    <h2 className="card-number">
-                        <Protect plan="premium" fallback="Free">
-                          Premium
-                        </Protect>
-                    </h2>              
-                </div>
-                <div className="card-icon">
-                    <Sparkles className="sparkles-icon" />
+            <div className="cards-wrapper">
+              {/* Active Plan Card */}
+                <div className="card">
+                    <div className="card-text">
+                        <p className="card-label">Active Plan</p>
+                        <h2 className="card-number">
+                            <Protect plan="premium" fallback="free">
+                              Premium
+                            </Protect>
+                        </h2>              
+                    </div>
+                    <div className="card-icon">
+                        <Gem className="sparkles-icon" />
+                    </div>
                 </div>
             </div>
-          </div>
+
+            <div className='recent-creations'>
+                <p className='recent-title'>Recent Creations</p>
+                {
+                  creations.map((item)=> <CreationItem key={item.id} item={item} />)
+                }
+            </div>
       </div>
 
       )
