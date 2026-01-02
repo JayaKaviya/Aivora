9a1efa7384fc874c43c2d2558dcecb9203cec04d
JayaKaviya <kavyamanz@gmail.com>
2026-01-02 16:24:37 +0530

    Server live updated & es module updated

---
+++ server/controllers/controller.js
@@ -0,0 +1,3 @@
+export const serverLive = (req, res) => {
+  res.status(200).send("Server is live");
+};
--- server/passport.js
+++ server/passport.js
@@ -1,27 +1,27 @@
-const GoogleStrategy = require('passport-google-oauth20').Strategy; 
-const passport =require('passport'); 
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 
+// Google OAuth strategy
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       callbackURL: "/auth/google/callback",
-      scope : ['profile', 'email']
+      scope: ["profile", "email"],
     },
-    function(accessToken, refreshToken, profile, callback){
-      // profile contains user info(photo,name,etc) from Google
+    function (accessToken, refreshToken, profile, callback) {
+      // profile contains user info (photo, name, etc) from Google
       // In app, you could save user info in DB here
       callback(null, profile);
     }
   )
-); 
-
+);
 
 // 5️⃣ Serialize & Deserialize user
 passport.serializeUser((user, done) => {
   done(null, user); // store entire user in session (or just user ID in real app)
-}); 
+});
 
 passport.deserializeUser((user, done) => {
   done(null, user); // retrieve user from session
